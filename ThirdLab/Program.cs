d67a8e328a804adea1c9a694be54aaae246ed165
ump1love <steam.max22.d74.74@gmail.com>
2023-09-24 13:23:22 +0300

    Rectangle class

---
+++ ThirdLab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        double side1;
+        double side2;
+
+        try
+        {
+            Console.Write("First side of the rectangle: ");
+            side1 = double.Parse(Console.ReadLine());
+            Console.Write("Second side of the rectangle: ");
+            side2 = double.Parse(Console.ReadLine());
+        }catch(Exception) { Console.WriteLine("Wrong input"); return; }
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+        double area = rectangle.Area;
+        double perimeter = rectangle.Perimeter;
+
+        Console.WriteLine($"Rectangle area: {area}\n" +
+                          $"Rectangle perimeter: {perimeter}");
+    }
+}
---
+++ ThirdLab/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Rectangle
+{
+    private double side1;
+    private double side2;
+
+
+    public Rectangle(double side1, double side2) 
+    { this.side1=side1; this.side2 = side2; }
+
+    public double CalculateArea() { return side1 * side2; }
+    public double CalculatePerimeter() { return (side1 + side2) * 2; }
+
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
